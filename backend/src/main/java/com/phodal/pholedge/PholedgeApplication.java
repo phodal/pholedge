1f3695e6e5dbb70efcead2c7f089468bd0940000
Phodal HUANG <h@phodal.com>
2019-10-14 09:17:39 +0800

    feat: init spring boot config

---
+++ backend/src/main/java/com/phodal/pholedge/PholedgeApplication.java
@@ -0,0 +1,13 @@
+package com.phodal.pholedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PholedgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PholedgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/phodal/pholedge/PholedgeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phodal.pholedge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PholedgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
