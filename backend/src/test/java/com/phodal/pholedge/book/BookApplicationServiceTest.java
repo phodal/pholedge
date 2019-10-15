f0b5fed0b03a1f6e871ce24aa130e7e650e3391d
Phodal HUANG <h@phodal.com>
2019-10-15 20:55:15 +0800

    tests: add test for service with mockito

---
+++ backend/src/test/java/com/phodal/pholedge/book/BookApplicationServiceTest.java
@@ -0,0 +1,39 @@
+package com.phodal.pholedge.book;
+
+import com.phodal.pholedge.book.model.Book;
+import com.phodal.pholedge.book.model.BookRepresentaion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+class BookApplicationServiceTest {
+    @InjectMocks
+    private BookApplicationService applicationService;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void should_return_book_representation_when_get_by_id() {
+        Book value = Book.create("123", "isbn", "name");
+        when(bookRepository.byId(anyString())).thenReturn(value);
+
+        BookRepresentaion represention = applicationService.getBookById("123");
+
+        assertEquals(represention.getName(), "name");
+    }
+}
