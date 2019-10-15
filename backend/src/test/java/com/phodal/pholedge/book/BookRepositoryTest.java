345b79422af327252e61e43e158602cbf956dcfe
Phodal HUANG <h@phodal.com>
2019-10-15 19:29:41 +0800

    fix: make tests works

--- backend/src/test/java/com/phodal/pholedge/book/BookRepositoryTest.java
+++ backend/src/test/java/com/phodal/pholedge/book/BookRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(BookRepository.class)
 class BookRepositoryTest {
     @Autowired(required = true)
     private BookRepository bookRepository;
