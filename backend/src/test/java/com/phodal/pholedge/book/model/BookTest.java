f0e30e83a99601a99118dd748efd0d28c6ad95df
Phodal HUANG <h@phodal.com>
2019-10-15 11:44:27 +0800

    feat: add teset for aggrateroot

---
+++ backend/src/test/java/com/phodal/pholedge/book/model/BookTest.java
@@ -0,0 +1,24 @@
+package com.phodal.pholedge.book.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void should_convert_to_presentation() {
+        Book afsBook = Book.create("123", "23424", "afs");
+        BookRepresentaion bookRepresentaion = afsBook.toRepresentation();
+        assertEquals(bookRepresentaion.getId(), afsBook.getId());
+        assertEquals(bookRepresentaion.getName(), afsBook.getName());
+    }
+//
+//    @Test
+//    void should_save_book() {
+//        Book afsBook = Book.create("123", "23424", "afs");
+//        afsBook.save("phodal", "name");
+//        assertEquals(afsBook.getId(), "phodal");
+//        assertEquals(afsBook.getName(), "name");
+//    }
+}
