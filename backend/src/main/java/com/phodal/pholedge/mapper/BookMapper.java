753bd765e532dd75e80e1bbd3c73808efd01d624
Phodal HUANG <h@phodal.com>
2019-10-15 16:23:24 +0800

    fix: fix mapper naming issue

--- backend/src/main/java/com/phodal/pholedge/mapper/BookMapper.java
+++ backend/src/main/java/com/phodal/pholedge/mapper/BookMapper.java
@@ -2,21 +2,16 @@ package com.phodal.pholedge.mapper;
 
 import com.phodal.pholedge.book.model.Book;
 import com.phodal.pholedge.book.model.BookRepresentaion;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public interface BookMapper {
-    @Insert("INSERT INTO book (id, isbn, name, createdAt) VALUES (#{id}, #{isbn}, #{name}, #{createdAt})" +
-            "ON DUPLICATE KEY UPDATE isbn=#{isbn}, name=#{name}")
-    int doSave(Book book);
 
-    @Select("select * from book order by id")
+    void doSave(Book book);
+
     List<BookRepresentaion> list();
 
-    @Select("select * from book where id = #{id}")
     Book byId(String id);
 }
