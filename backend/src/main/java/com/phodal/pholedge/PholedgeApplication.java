ef069fd5c69cb5ed33bde5a936eebfb0144a7658
Phodal HUANG <h@phodal.com>
2019-10-17 11:41:52 +0800

    build: add feign to deps

--- backend/src/main/java/com/phodal/pholedge/PholedgeApplication.java
+++ backend/src/main/java/com/phodal/pholedge/PholedgeApplication.java
@@ -2,6 +2,7 @@ package com.phodal.pholedge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -10,6 +11,7 @@ import static java.time.ZoneId.of;
 import static java.util.TimeZone.getTimeZone;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PholedgeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PholedgeApplication.class, args);
