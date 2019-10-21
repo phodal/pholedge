bedc15107bbc462ac5744b64cda6428c6217e2e6
Phodal HUANG <h@phodal.com>
2019-10-21 23:51:23 +0800

    tests: init rest assured to test repository

--- backend/src/test/java/com/phodal/pholedge/blog/BlogControllerTest.java
+++ backend/src/test/java/com/phodal/pholedge/blog/BlogControllerTest.java
@@ -1,68 +1,43 @@
 package com.phodal.pholedge.blog;
 
-import com.phodal.pholedge.blog.command.SaveBlogCommand;
+import com.phodal.pholedge.BaseApiTest;
 import com.phodal.pholedge.blog.command.SavePlayCommand;
-import com.phodal.pholedge.blog.model.Blog;
+import com.phodal.pholedge.blog.model.WdsmPlay;
+import com.phodal.pholedge.blog.repository.BlogRepository;
+import com.phodal.pholedge.blog.repository.WdsmPlayClient;
 import com.phodal.pholedge.common.PholedgeTestUtils;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.context.annotation.Import;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(BlogController.class)
 @AutoConfigureMybatis
-class BlogControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+@Import(BlogRepository.class)
+class BlogControllerTest extends BaseApiTest {
+    @Mock
+    private WdsmPlayClient wdsmPlayClient;
 
-    @MockBean
-    private BlogService blogService;
-
-    @Test
-    public void should_return_default_data() throws Exception {
-        Blog blog = Blog.create("213", "https://www.phodal.com", "title", "json");
-        given(blogService.savePlay(any())).willReturn(blog.getId());
-
-        SavePlayCommand savePlayCommand = new SavePlayCommand("195", "234");
-
-        this.mockMvc.perform(
-                post("/blogs/savePlay")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("utf-8")
-                        .content(PholedgeTestUtils.asJsonString(savePlayCommand))
-        )
-                .andExpect(status().isCreated())
-                .andExpect(content().string("{\"id\":\"213\"}"));
-    }
+    @Autowired(required = true)
+    private BlogRepository blogRepository;
 
     @Test
-    public void should_return_empty_when_error_id() throws Exception {
-        Blog blog = Blog.create("213", "https://www.phodal.com", "title", "json");
-//        given(blogService.savePlay(any())).willReturn(blog.getId());
+    public void should_verify_api_exists() {
+        WdsmPlay wdsmPlay = new WdsmPlay(222, "222", "2222", "title", "slug", "adaf", "2019-10-21", "afasdf", "12");
+        Mockito.when(wdsmPlayClient.getPlay("222")).thenReturn(wdsmPlay);
 
         SavePlayCommand savePlayCommand = new SavePlayCommand("222", "234");
 
-        this.mockMvc.perform(
-                post("/blogs/savePlay")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("utf-8")
-                        .content(PholedgeTestUtils.asJsonString(savePlayCommand))
-        )
-                .andExpect(status().isCreated())
-                .andExpect(content().string("{\"id\":\"213\"}"));
-    }
+        given()
+                .contentType(ContentType.JSON)
+                .body(PholedgeTestUtils.asJsonString(savePlayCommand))
 
+                .when()
+                .post("/blogs/savePlay")
+                .then()
+                .statusCode(500);
+    }
 }
